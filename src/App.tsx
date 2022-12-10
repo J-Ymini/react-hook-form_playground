c189afa23747ef9fa4a3a30fc1ea77c32eebc974
yongminJeon <dydalsdl1414@gmail.com>
2022-12-10 17:08:06 +0900

    feat: add controller, yup resolver

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useForm } from 'react-hook-form';
+import FormTest from './FormTest';
+import ControllerTest from './ControllerTest';
 
 function App() {
-  type FormValues = {
-    firstName: string;
-    lastName: string;
-    age: number;
-    gender: string;
-  };
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: {
-      isValid,
-      isDirty,
-      dirtyFields,
-      errors: {
-        firstName: firstNameError,
-        lastName: lastNameError,
-        age: ageError,
-        gender: genderError,
-      },
-    },
-  } = useForm<FormValues>();
-
-  console.log('isValid', isValid);
-  console.log('isDirty', isDirty);
-  console.log('dirtyFields', dirtyFields);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form
-          className="flex flex-col  text-black"
-          onSubmit={handleSubmit(console.log)}
-        >
-          <label className="text-white text-left text-bold" htmlFor="FirstName">
-            First name
-          </label>
-          <input
-            className="mb-4 p-3 rounded-lg"
-            placeholder="FirstName"
-            id="FirstName"
-            {...register('firstName', { required: 'This is required' })}
-          />
-          {firstNameError && (
-            <div className="text-red-500">{firstNameError.message}</div>
-          )}
-          <label className="text-white text-left text-bold" htmlFor="LastName">
-            LastName
-          </label>
-          <input
-            className="mb-4 p-3 rounded-lg"
-            placeholder="LastName"
-            id="LastName"
-            {...register('lastName', {
-              required: true,
-              maxLength: {
-                value: 4,
-                message: 'You exceeded the maximum value',
-              },
-            })}
-          />
-          {lastNameError && (
-            <div className="text-red-500">{lastNameError.message}</div>
-          )}
-          <label className="text-white text-left text-bold" htmlFor="Age">
-            Age
-          </label>
-          <input
-            className="mb-4 p-3 rounded-lg"
-            placeholder="Age"
-            id="Age"
-            {...register('age', { required: true, valueAsNumber: true })}
-          />
-          <select
-            className="p-3 rounded-lg mb-4"
-            id="Gender"
-            {...register('gender', { required: true })}
-          >
-            <option>male</option>
-            <option>female</option>
-          </select>
-          <button
-            className="bg-blue-500 text-bold text-white rounded-lg p-3"
-            disabled={!isValid}
-          >
-            Submit
-          </button>
-        </form>
       </header>
+      <div className="max-w-md mx-auto">
+        {/* <FormTest /> */}
+        <ControllerTest />
+      </div>
     </div>
   );
 }
---
+++ src/ControllerTest.tsx
@@ -0,0 +1,72 @@
+import { Button, Input, Space } from 'antd';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+type FormType = {
+  id: string;
+  password: string;
+};
+
+const schema = yup.object().shape({
+  id: yup.string().required('this is required(id)').min(3, 'min 3 char'),
+  password: yup
+    .string()
+    .required('this is required(password)')
+    .max(5, 'max 5 char'),
+});
+
+const ControllerTest = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isValid },
+  } = useForm<FormType>({ mode: 'all', resolver: yupResolver(schema) });
+
+  console.log(typeof schema);
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        console.log(data);
+      })}
+    >
+      <Space direction="vertical">
+        <div>
+          <Controller
+            name="id"
+            control={control}
+            render={({ field }) => (
+              <Input size="large" placeholder="ID" {...field} />
+            )}
+          />
+          <div className="text-red-500">{errors.id && errors.id.message}</div>
+        </div>
+        <div>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <Input.Password size="large" placeholder="PASSWORD" {...field} />
+            )}
+          />
+          <div className="text-red-500">
+            {errors.password && errors.password.message}
+          </div>
+        </div>
+        <Button
+          disabled={!isValid}
+          type="primary"
+          className="bg-blue-500 text-white w-full disabled:bg-gray-500"
+          htmlType="submit"
+        >
+          Submit
+        </Button>
+      </Space>
+    </form>
+  );
+};
+
+export default ControllerTest;
